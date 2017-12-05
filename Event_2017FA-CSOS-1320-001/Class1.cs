0659f867fea687d89c0656a86a7ea712a19a0e39
jarly <jarly@JARRETT-XPS-15>
2017-12-05 13:36:31 -0600

    Add project files.

---
+++ Event_2017FA-CSOS-1320-001/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_2017FA_CSOS_1320_001
+{
+    public class Class1
+    {
+    }
+}
